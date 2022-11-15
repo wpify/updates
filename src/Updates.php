65811dc67e66f2e930e086488695605d137fe0bc
Václav Greif <info@vaclavgreif.cz>
2022-11-15 17:56:20 +0100

    Update deps

--- src/Updates.php
+++ src/Updates.php
@@ -2,13 +2,15 @@
 
 namespace Wpify\Updates;
 
+use YahnisElsts\PluginUpdateChecker\v5\PucFactory;
+
 class Updates {
 
 	private $plugin_file;
 	private $plugin_slug;
 	private $extra_data;
 
-	public function __construct( string $plugin_file, string $plugin_slug, array $extra_data = []) {
+	public function __construct( string $plugin_file, string $plugin_slug, array $extra_data = [] ) {
 		$this->plugin_file = $plugin_file;
 		$this->plugin_slug = $plugin_slug;
 		$this->extra_data  = $extra_data;
@@ -23,7 +25,7 @@ class Updates {
 		$url = sprintf( 'https://wpify.io/?update_action=get_metadata&update_slug=%s&site_url=%s', $this->plugin_slug, site_url() );
 		$url = add_query_arg( $this->extra_data, $url );
 
-		\Puc_v4_Factory::buildUpdateChecker(
+		PucFactory::buildUpdateChecker(
 			$url,
 			$this->plugin_file,
 			$this->plugin_slug
